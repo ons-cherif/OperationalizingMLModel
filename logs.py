21ad03f5de6b5ffb285607dc4cd28e29d8a01f9d
Ons CHERIF <77125529+ons-cherif@users.noreply.github.com>
2021-02-06 19:51:45 +0100

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,17 @@
+from azureml.core import Workspace
+from azureml.core.webservice import Webservice
+
+# Requires the config to be downloaded first to the current working directory
+ws = Workspace.from_config()
+
+# Set with the deployment name
+name = "deploybestmodel"
+
+# load existing web service
+service = Webservice(name=name, workspace=ws)
+logs = service.get_logs()
+
+service.update(enable_app_insights= True)
+
+for line in logs.split('\n'):
+    print(line)
