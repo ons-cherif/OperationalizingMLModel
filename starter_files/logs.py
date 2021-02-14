6b29b1f71224187724867c7d563703b731aee3e9
Ons CHERIF <77125529+ons-cherif@users.noreply.github.com>
2021-02-14 11:21:14 +0100

    Update logs.py

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,11 +5,13 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "deploybestmodel"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
 logs = service.get_logs()
+
+service.update(enable_app_insights=True)
 
 for line in logs.split('\n'):
     print(line)
